67116a0f9d04c32c7285494043b3fc7d27f457c8
1vernuclear <emil1093@outlook.com>
2023-05-17 10:23:30 +0200

    Small project structure changes

---
+++ 17_CDIO/src/test/java/LineCreationTest.java
@@ -0,0 +1,15 @@
+package java;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LineCreationTest{
+
+
+    @Test
+    public void testline(){
+
+    }
+
+
+}
