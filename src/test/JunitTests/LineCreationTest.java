1767518f44d5735804ef5e42946fa9833e5fa074
1vernuclear <emil1093@outlook.com>
2023-05-17 12:51:41 +0200

    Small project structure changes

--- src/test/JunitTests/LineCreationTest.java
+++ src/test/JunitTests/LineCreationTest.java
@@ -1,12 +1,9 @@
-package java;
-
+package JunitTests;
 
 import LineCreation.ConstructLine;
 import LineCreation.LineSegment;
-import org.junit.Test;
+import org.junit.*;
 import org.opencv.core.Point;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class LineCreationTest{
 
@@ -15,7 +12,7 @@ public class LineCreationTest{
     @Test
     public void testLine(){
         LineSegment lineSegment = line.constructLine(new Point(50,150), new Point(150,50),720,480);
-        assertEquals(new Point(200,0),lineSegment.getEndPoint());
+        Assert.assertEquals(new Point(200,0),lineSegment.getEndPoint());
     }
 
 
