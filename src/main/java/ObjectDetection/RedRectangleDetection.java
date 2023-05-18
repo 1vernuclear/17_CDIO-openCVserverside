7258060c148ff7b067231fab82067de92375ae6f
1vernuclear <emil1093@outlook.com>
2023-05-18 10:51:05 +0200

    Initial setup of capturing 1 frame from the videocapture

--- src/main/java/ObjectDetection/RedRectangleDetection.java
+++ src/main/java/ObjectDetection/RedRectangleDetection.java
@@ -1,5 +1,60 @@
 package ObjectDetection;
 
+import org.bytedeco.javacv.CanvasFrame;
+import org.bytedeco.javacv.FrameGrabber;
+import org.bytedeco.javacv.OpenCVFrameConverter;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.videoio.VideoCapture;
+
+import java.io.File;
+import java.net.URL;
+
 public class RedRectangleDetection {
+
+    public RedRectangleDetection(VideoCapture videoCapture){
+        detectField(videoCapture);
+    }
+
+    public void detectField(VideoCapture videoCapture){
+
+        retrieveFrame(videoCapture);
+
+    }
+
+    public void retrieveFrame(VideoCapture videoCapture){
+        // Check if the VideoCapture object is opened successfully
+        if (!videoCapture.isOpened()) {
+            System.out.println("Failed to open the webcam.");
+            return;
+        }
+
+        // Read a frame from the video capture
+        Mat frame = new Mat();
+        if (videoCapture.read(frame)) {
+            // Save the frame as a PNG file
+            String imagePath = getRessourcePath();
+            Imgcodecs.imwrite(imagePath, frame);
+            System.out.println("Frame saved as " + imagePath);
+        } else {
+            System.out.println("Failed to capture a frame.");
+        }
+    }
+
+    private String getRessourcePath(){
+        // Get the resource path
+        URL resourceUrl = RedRectangleDetection.class.getClassLoader().getResource("resources");
+
+        String resourcePath = null;
+
+        // Check if the resource URL is not null
+        if (resourceUrl != null) {
+            // Convert the resource URL to a file path
+            resourcePath = new File(resourceUrl.getFile()).getAbsolutePath() + "/FieldImages/runtimeimage.png";
+
+        }
+
+        return (resourcePath != null) ? resourcePath : "file not found";
+    }
 
 }
--- src/main/java/main/java/Main.java
+++ src/main/java/main/java/Main.java
@@ -1,11 +1,28 @@
 package main.java;
 
 import org.opencv.core.Core;
+import ObjectDetection.*;
+import org.opencv.videoio.VideoCapture;
 
 public class Main {
-    public static void main(String[] args) {
+
+    //load the opencv library into the JVM at runtime
+    static {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
+    public static void main(String[] args) {
+        //print current version of opencv
         System.out.println(Core.VERSION);
+
+        // Open the video capture
+        VideoCapture videoCapture = new VideoCapture(0);
+
+        RedRectangleDetection detectField = new RedRectangleDetection(videoCapture);
+
+        //stop capturing
+        videoCapture.release();
+
     }
 }
 
